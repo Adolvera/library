5ff135038ea2f3020dfb034667e71528f77f8669
Adolvera <aolvera4734@gmail.com>
2022-03-29 15:29:46 -0400

    Worked on form validation being more concise

--- main.js
+++ main.js
@@ -1,15 +1,16 @@
 const formData = document.getElementById("addBookForm");
-const submitButton = formData.querySelector("button[type='submit']");
+const submitButton = formData.querySelector("button[type='button']");
 const titleInput = formData.querySelector("#title");
 const authorInput = formData.querySelector("#author");
 const pagesInput = formData.querySelector("#pages");
 const readCheckbox = formData.querySelector("#read");
 const addBookButton = document.querySelector("#add-button");
 const formDiv = document.querySelector("#div-form");
+const checkboxDiv = formData.querySelector("#checkbox-div");
 
 submitButton.addEventListener('click', (e) => {
   const validCheck = checkValid(titleInput.value, authorInput.value, pagesInput.value);
-  if (validCheck === true) {
+  if (validCheck) {
     addBookToLibrary(titleInput.value, authorInput.value, pagesInput.value, readCheckbox.checked);
     toggleForm();
     showBooks();
@@ -24,6 +25,16 @@ let bookArray = [{
   pages: 240,
   read: false
 }];
+
+let errorDiv = document.createElement('div');
+let errorMessage = document.createElement('p');
+const errorIcon = document.createElement('img');
+errorIcon.src = "img/error.png";
+errorIcon.classList.add("icon");
+errorDiv.appendChild(errorIcon);
+errorDiv.appendChild(errorMessage);
+errorDiv.classList.add("form-div", "error", "error-flex");
+checkboxDiv.after(errorDiv);
 
 function Book(title, author, pages, read) {
   this.title = title;
@@ -49,23 +60,28 @@ const toggleForm = () => {
 }
 
 const showBooks = (bookArray) => {
-  bookArray.forEach((book) => {
-
-  })
 }
 
 const checkValid = (title, author, pages) => {
-  if (bookArray.indexOf(title) !== -1) {
+  const repeatName = bookArray.some(book => book.title === title);
+  errorDiv.classList.add("form-div");
+  if (title === "") {
+    errorMessage.textContent = "Please enter a title"
+    errorDiv.classList.remove("form-div");
+    return false;
+  } else if (repeatName) {
+    errorMessage.textContent = "Title has already been used"
+    errorDiv.classList.remove("form-div");
+    return false;
+  } else if (!author) {
+    errorMessage.textContent = "Please enter an author"
+    errorDiv.classList.remove("form-div");
+    return false;
+  } else if (pages === "0" || pages === "") {
+    errorMessage.textContent = "Please insert amount of pages"
+    errorDiv.classList.remove("form-div");
     return false;
   }
-
-  if (!author) {
-    return false;
-  }
-
-  if (!pages) {
-    return false;
-  }
-
+  
   return true;
 }
